22e378e9850eee62c7199e23509b4d6908b72dbe
Brad Koch <brad@farmlogs.com>
2015-10-15 18:04:30 -0400

    Added correct metadata to project.clj

--- project.clj
+++ project.clj
@@ -1,7 +1,6 @@
-(defproject bugsnag-tools "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:url "http://www.eclipse.org/legal/epl-v10.html"}
+(defproject bugsnag-tools "0.1.0"
+  :description "Tools for querying the Bugsnag API."
+  :url "https://github.com/FarmLogs/bugsnag-tools"
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [clj-http "2.0.0"]
                  [cheshire "5.5.0"]])
